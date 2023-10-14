679beb7ad0ad3818e84ce971fb2dd01261c3a33b
aushtiin <aotangban@gmail.com>
2023-10-14 12:50:15 +0100

    Initial Commit

---
+++ PostIT/PostITApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostITApp.swift
+//  PostIT
+//
+//  Created by Austin  Tangban on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PostITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
